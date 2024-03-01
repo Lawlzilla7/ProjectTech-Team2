7171b2ced00a8a2eee9d6976e11113eba93e454d
Sindydejong <sindymdejong@gmail.com>
2024-03-01 10:48:50 +0100

    Basis
    De gehele basis, met gekoppelde database

---
+++ server.js
@@ -0,0 +1,107 @@
+// require('dotenv').config()
+// const path = require('path');
+// require('dotenv').config({
+//   path: path.resolve('config.env'),
+// });
+
+const express = require('express')
+const app = express()
+
+app 
+	.set('view engine', 'ejs') // Set EJS to be our templating engine
+	.set('views', 'views')  // And tell it the views can be found in the directory named views
+	.use(express.urlencoded({extended: true})) // middleware to parse form data from incoming HTTP request and add form fields to req.body
+	.use(express.static('static'))             // Allow server to serve static content such as images, stylesheets, fonts or frontend js from the directory named static
+	.get('/', onhome)
+    .get('/about/:name', onabout)
+	.listen(8000)
+
+// Use MongoDB
+const { MongoClient, ServerApiVersion, ObjectId, CommandStartedEvent } = require('mongodb')
+// Construct URL used to connect to database from info in the .env file
+const uri = "mongodb+srv://sindy:mongo123@clustertech.5fqnsm1.mongodb.net/?retryWrites=true&w=majority&appName=ClusterTech";
+// Create a MongoClient
+const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+})
+
+// Try to open a database connection
+client.connect()
+    .then((res) => {
+        console.log('Database connection established')
+    })
+    .catch((err) => {
+        console.log(`Database connection error - ${err}`)
+        console.log(`For uri - ${uri}`)
+    })
+
+
+function onhome(req, res) {
+	res.render('pages/index')
+}
+
+function onabout(req, res) {
+    render.render('pages/index')
+	res.send (`<h1> About ${req.params.name} </h1>`)
+}
+
+
+
+
+app.get('/add', showAddForm) //middleware: parses form data
+app.post('/add-movie', addMovie) //Route to handle the post request to /add-movie
+
+function showAddForm(req, res) {
+  res.render('pages/add.ejs')
+}
+
+function addMovie(req,res) {
+	res.send(`thanks for adding the movie with:
+	title: ${req.body.title},
+	plot: ${req.body.plot},
+	and descriptrsion:  ${req.body.description}
+ `)
+}
+
+
+async function run() {
+	try {
+	  const database = client.db('sample_mflix');
+	  const movies = database.collection('movies');
+	  // Query for a movie that has the title 'Back to the Future'
+	  const query = { title: 'Back to the Future' };
+	  const movie = await movies.findOne(query);
+	  console.log(movie);
+	} finally {
+	  // Ensures that the client will close when you finish/error
+	  await client.close();
+	}
+  }
+  run().catch(console.dir);
+  
+
+
+// Middleware to handle not found errors - error 404
+app.use((req, res) => {
+	// log error to console
+	console.error('404 error at URL: ' + req.url)
+	// send back a HTTP response with status code 404
+	res.status(404).send('Sorry foutje! 404 error at URL: ' + req.url)
+  })
+  
+  // Middleware to handle server errors - error 500
+  app.use((err, req, res) => {
+	// log error to console
+	console.error(err.stack)
+	// send back a HTTP response with status code 500
+	res.status(500).send('500: server error')
+  })
+
+  // Start the webserver and listen for HTTP requests at specified port
+app.listen(process.env.PORT, () => {
+	console.log(`I did not change this message and now my webserver is listening at port ${process.env.PORT}`)
+  })
