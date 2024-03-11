b8bcfbf170ab346e3a1b4f28dbfa3732c18096e6
Furkan Tura <ftura7@gmail.com>
2024-03-11 17:42:16 +0100

    dingen

--- scripts/script.js
+++ scripts/script.js
@@ -1,51 +1,45 @@
-import * as Swiper from 'swiper';
+document.addEventListener('DOMContentLoaded', function () {
+    var swiper1 = new Swiper('#swiper1', {
+        speed: 300,
+        cssMode: true, 
+        navigation: {
+            nextEl: '#swiper1 .swiper-button-next',
+            prevEl: '#swiper1 .swiper-button-prev',
+        },
+    });
 
+    var swiper2 = new Swiper('#swiper2', {
+        speed: 300,
+        cssMode: true, 
+        navigation: {
+            nextEl: '#swiper2 .swiper-button-next',
+            prevEl: '#swiper2 .swiper-button-prev',
+        },
+    });
 
-var options = {
-    direction: 'horizontal',
-    loop: 'true',
-    speed: 300,
-    cssMode: true,
+    var swiper3 = new Swiper('#swiper3', {
+        speed: 300,
+        cssMode: true,    
+        navigation: {
+            nextEl: '#swiper3 .swiper-button-next',
+            prevEl: '#swiper3 .swiper-button-prev',
+            
+        },
+    });
+});
+// var options = {
+//     direction: 'horizontal',
+//     loop: 'true',
+//     speed: 300,
+//     cssMode: true,
 
-    pagination: {
-        el: '.swiper-pagination',
-        type: 'fraction'
-    },
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev'
-    }
+//     pagination: {
+//         el: '.swiper-pagination',
+//         type: 'fraction'
+//     },
+//     navigation: {
+//         nextEl: '.swiper-button-next',
+//         prevEl: '.swiper-button-prev'
+//     }
 
-};
-
-
-
-// eslint-disable-next-line no-undef
-// Document.addEventListener('DOMContentLoaded', function () {
-//     var swiper1 = new Swiper('#swiper1', {
-//         direction: 'horizontal',
-//         loop: true,
-//         navigation: {
-//             nextEl: '.swiper-button-next',
-//             prevEl: '.swiper-button-prev',
-//         },
-//     });
-
-//     var swiper2 = new Swiper('#swiper2', {
-//         direction: 'horizontal',
-//         loop: true,
-//         navigation: {
-//             nextEl: '.swiper-button-next',
-//             prevEl: '.swiper-button-prev',
-//         },
-//     });
-
-//     var swiper3 = new Swiper('#swiper3', {
-//         direction: 'horizontal',
-//         loop: true,
-//         navigation: {
-//             nextEl: '.swiper-button-next',
-//             prevEl: '.swiper-button-prev',
-//         },
-//     });
-// });
+// };
